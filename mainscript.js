e7a6386d265f27bf1a790034891edc965d4e6845
Unjustifiable1 <cupidojustin7@gmail.com>
2023-11-21 11:54:32 +0200

    add options menu to ui

---
+++ mainscript.js
@@ -0,0 +1,10 @@
+// Tabletennis UI
+
+function ttOptions() {
+    var x = document.getElementById("ttMenu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
